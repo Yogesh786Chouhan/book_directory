616d8fd3c59ca118e56dbf919288994d5d66e7d6
Yogesh786Chouhan <129957831+Yogesh786Chouhan@users.noreply.github.com>
2024-07-23 18:07:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-require('dotenv').config();
+// require('dotenv').config();
 const router = require('./router/routes');
 const AdminModel = require('./model/admin_model');
 const category = require('./model/category_model')
@@ -13,17 +13,16 @@ const path = require('path')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+//config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require('dotenv').config();
 
+}
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
 })
-
-
-
-
-
 
 app.use(cors());
 app.use('/public', express.static('public'))
@@ -32,14 +31,3 @@ app.listen(process.env.PORT, () => {
     console.log(`Book_directory server is working on port ${process.env.PORT}`);
 })
 
-
-
-
-// For rendering purpose
-
-// "scripts": {
-//     "test": "echo \"Error: no test specified\" && exit 1",
-//     "frontend":"cd frontend && npm start",
-//     "backend":"cd backend && npm start",
-//     "dev": "concurrently \"npm run frontend\" \"npm run backend\""
-//   },
